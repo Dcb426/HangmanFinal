4c6beab6e1a5aae2eea5cec3402624616f99295d
Darius Bell <dcb13a@lib-301-07.local>
2018-03-26 12:55:40 -0500

    final push

--- llloo/ViewController.swift
+++ llloo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController,UITextFieldDelegate {
 
@@ -18,7 +19,9 @@ class ViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var textentry: UITextField!
     @IBOutlet weak var PictureToBeChanged: UIImageView!
     @IBOutlet weak var change: UILabel!
-    var Phrases = ["which","there","their","about","would","these","other","words","could","write","first","water","after","where","right","think","three","years","place","sound","great"]
+    @IBOutlet weak var resetbutton: UIButton!
+    var backgroundmusicplayer = AVAudioPlayer()
+    var Phrases = ["which","there","their","about","would","these","other","words","could","write","first","water","after","where","right","think","three","years","place","sound","great","ADORE", "AGILE", "AGREE", "ALERT", "ALIVE", "ALLOW", "ALOHA", "AMAZE", "AMITY", "AMPLE", "AMPLY", "AMUSE", "ANGEL", "ARDOR", "AWARE","Taner","Sucks","Natap", "Darius","Drake"]
     var Imagesets = ["Hang_00","Hang_01","Hang_02","Hang_03","Hang_04","Hang_05","Hang_06","Hang_07"]
     var livesCheck = 1
     var phrase_check = ""
@@ -27,6 +30,7 @@ class ViewController: UIViewController,UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         textentry.delegate = self
+        playBackgroundmusic(fileNamed: "saxman.mp3")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -45,49 +49,89 @@ class ViewController: UIViewController,UITextFieldDelegate {
     @IBAction func buttonPressed(sender: AnyObject)
     {
         let randomIndex = Int(arc4random_uniform(UInt32(Phrases.count)))
-        randomItem = Phrases[randomIndex]
+        randomItem = Phrases[randomIndex].lowercased()
         PictureToBeChanged.image = UIImage(named: Imagesets[1]);
+        textentry.isUserInteractionEnabled = true
+        X1.text = "X"
+        X2.text = "X"
+        X3.text = "X"
+        X4.text = "X"
+        X5.text = "X"
+        livesCheck = 1
+        resetbutton.setTitle("Start", for: .normal)
         //change.text = randomItem
         //*******
         //make sure to basic start up functionality
         //*****
     }
     @IBAction func EventTriggered(_ sender: Any) {
-        let text: String = textentry.text!
-        var chars = [Character](text)
-        var charholder = [Character](randomItem)
-        if(randomItem.contains(chars[0]))
-        {
-            if(charholder[0] == chars[0])
+            let text: String = textentry.text!
+            var chars = [Character](text)
+            var charholder = [Character](randomItem)
+            if(randomItem.contains(chars[0]))
             {
-                X1.text = text
+                if(charholder[0] == chars[0])
+                {
+                    X1.text = text
+                }
+                if(charholder[1] == chars[0])
+                {
+                    X2.text = text
+                }
+                if(charholder[2] == chars[0])
+                {
+                    X3.text = text
+                }
+                if(charholder[3] == chars[0])
+                {
+                    X4.text = text
+                }
+                if(charholder[4] == chars[0])
+                {
+                    X5.text = text
+                }
+                textentry.text = nil
+                if(X1.text != "X" && X2.text != "X" && X3.text != "X" && X4.text != "X" && X5.text != "X")
+                {
+                    change.text = "You Win"
+                    resetbutton.setTitle("Reset", for: .normal)
+                    textentry.isUserInteractionEnabled = false
+                }
             }
-            if(charholder[1] == chars[0])
+            else
             {
-                X2.text = text
-            }
-            if(charholder[2] == chars[0])
-            {
-                X3.text = text
-            }
-            if(charholder[3] == chars[0])
-            {
-                X4.text = text
-            }
-            if(charholder[4] == chars[0])
-            {
-                X5.text = text
+                if(livesCheck<6)
+                {
+                    livesCheck+=1
+                    PictureToBeChanged.image = UIImage(named: Imagesets[livesCheck]);
+                    textentry.text = nil
+                }
+                else
+                {
+                    change.text = "You Lose"
+                    resetbutton.setTitle("Reset", for: .normal)
+                    textentry.isUserInteractionEnabled = false
+                }
             }
         }
-        else
-        {
-            livesCheck+=1
-            PictureToBeChanged.image = UIImage(named: Imagesets[livesCheck]);
+    func playBackgroundmusic (fileNamed: String)
+    {
+        let url = Bundle.main.url(forResource: fileNamed, withExtension: nil)
+        guard let newUrl = url else {
+            print("Could Not Find")
+            return
         }
-        
-
+        do {
+            backgroundmusicplayer = try AVAudioPlayer(contentsOf: newUrl)
+            backgroundmusicplayer.numberOfLoops = -1
+            backgroundmusicplayer.prepareToPlay()
+            backgroundmusicplayer.play()
+        }
+        catch let error as NSError {
+            print(error.description)
+        }
     }
-
+    
 
 }
 
